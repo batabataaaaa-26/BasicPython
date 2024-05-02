1033d0826e00001604fc99f6702e6c9043deb122
batabataaaaa-26 <batabata02261010@gmail.com>
2024-05-02 22:05:20 +0900

    modified_1

--- euclid.py
+++ euclid.py
@@ -1,4 +1,12 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+a = int(input("a の値を入力: "))
+b = int(input("b の値を入力: "))
 
 # TODO
+def gcd(a, b):
+    while b != 0:
+        tmp = b
+        b = a % b
+        a = tmp
+    return a
+
+print("({}, {})の最大公約数は{}".format(a, b, gcd(a, b)))
